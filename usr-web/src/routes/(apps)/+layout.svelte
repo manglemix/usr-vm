34dfedb6d1fa31041060dee311337399791a8941
Najman Husaini <najmanhusaini20@gmail.com>
2025-01-14 14:19:08 -0700

    fixes

--- usr-web/src/routes/(apps)/+layout.svelte
+++ usr-web/src/routes/(apps)/+layout.svelte
@@ -5,11 +5,11 @@
 
 <header class="fixed top-0 w-full p-2 shadow">
 	<nav class="flex flex-row items-center gap-4">
-		<a href={`/${base}`}>
+		<a href={`${base}`}>
 			<enhanced:img src="$lib/icon.png" alt="Utah Student Robotics" class="logo" />
 		</a>
-		<a href={`/${base}/scheduler`}>Scheduler</a>
-		<!-- <a href={`/${base}/manifest`}>Manifest</a> -->
+		<a href={`${base}/scheduler`}>Scheduler</a>
+		<!-- <a href={`${base}/manifest`}>Manifest</a> -->
 	</nav>
 </header>
 
