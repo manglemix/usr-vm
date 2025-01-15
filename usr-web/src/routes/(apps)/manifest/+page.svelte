317b1773e57d96e84d9cee490cabbc3b954ff854
Najman Husaini <najmanhusaini20@gmail.com>
2025-01-15 11:43:35 -0700

    corrected date order in csv

--- usr-web/src/routes/(apps)/manifest/+page.svelte
+++ usr-web/src/routes/(apps)/manifest/+page.svelte
@@ -118,7 +118,7 @@
 					const day = String(date.getDate()).padStart(2, '0');
 					const month = String(date.getMonth() + 1).padStart(2, '0');
 					const year = String(date.getFullYear()).slice(-2);
-					return `${s.status}: ${day}/${month}/${year}`;
+					return `${s.status}: ${month}/${day}/${year}`;
 				})
 				.join(',');
 			const sub = (o.count * (o.unit_cost as number)).toFixed(2);
